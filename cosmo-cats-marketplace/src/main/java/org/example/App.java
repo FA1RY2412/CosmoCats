7a5eceb4144fbd318f5f03a3612b79bdfce3d296
FA1RY2412 <denisgolub2412@gmail.com>
2025-10-13 13:06:17 +0300

    Update App.java

--- cosmo-cats-marketplace/src/main/java/org/example/App.java
+++ cosmo-cats-marketplace/src/main/java/org/example/App.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication(scanBasePackages = "org.example")
 public class App {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+
+  // друк усіх замаплених ендпоінтів при старті
+  @Bean
+  CommandLineRunner printRoutes(RequestMappingHandlerMapping mapping) {
+    return args -> mapping.getHandlerMethods().forEach((info, method) ->
+        System.out.println(">> " + info));
   }
 }
